54c03ec812e970b79d147e784e5f986742b1be1f
sammysundar2001@gmail.com <samradnus2001@gmail.com>
2022-05-17 08:51:47 +0530

    Added Display Topic,User and Hosts

--- base/views.py
+++ base/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import render
+from .models import Room
 # Create your views here.
 
 
-rooms = [
-    {'id':1,'name':'lets learn python!'},
-    {'id':2,'name':'lets learn django!'},
-    {'id':3,'name':'lets learn flask!'},
-]
+# rooms = [
+#     {'id':1,'name':'lets learn python!'},
+#     {'id':2,'name':'lets learn django!'},
+#     {'id':3,'name':'lets learn flask!'},
+# ]
 def home(request):
-        context={'rooms':rooms}
-        return render(request,'base/home.html',context)
-
+    #objects-model manager
+    rooms=Room.objects.all()
+    context={'rooms':rooms}
+    return render(request,'base/home.html',context)
+#pk=primary key
 def room(request,pk):
-    room=None
-    for i in rooms:
-        if i['id']==int(pk):
-            room=i
+    room=Room.objects.get(id=pk)
     context={'room':room}        
     return render(request,'base/room.html',context)   
+
+
