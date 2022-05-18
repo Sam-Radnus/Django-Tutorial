642fc2504566fd9eb0ebe886290a99a89a20a3d6
sammysundar2001@gmail.com <samradnus2001@gmail.com>
2022-05-18 23:03:06 +0530

    CRUD operation on messages

--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room,Topic
+from .models import Room,Topic,Message
 from .forms import RoomForm
 # Create your views here.
 
@@ -45,7 +45,6 @@ def logoutUser(request):
 
 def registerPage(request):
     form=UserCreationForm()
-
     if request.method=='POST':
         form=UserCreationForm(request.POST)
         if form.is_valid():
@@ -71,10 +70,22 @@ def home(request):
     room_count=rooms.count()
     context={'rooms':rooms,'topics':topics,'room_count':room_count}
     return render(request,'base/home.html',context)
+
 #pk=primary key
 def room(request,pk):
     room=Room.objects.get(id=pk)
-    context={'room':room}        
+    room_messages=room.message_set.all().order_by('-created')
+    participants=room.participants.all()
+    if request.method=='POST':
+       message=Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+       )
+       room.participants.add(request.user)
+       return redirect('room',pk=room.id)
+
+    context={'room':room,'room_messages':room_messages,'participants':participants}        
     return render(request,'base/room.html',context)   
 
 @login_required(login_url='login')
@@ -113,3 +124,12 @@ def deleteRoom(request,pk):
         return redirect('home')
     return render(request,'base/delete.html',{'obj':room})
 
+@login_required(login_url='login')
+def deleteMessage(request,pk):
+    message=Message.objects.get(id=pk)
+    if request.user!=message.user: #Redundant
+        return HttpResponse('Invalid Operation!!!')
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request,'base/delete.html',{'obj':message})
