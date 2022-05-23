ceac428ee11c8beb35f3d29be0cbf8cca4278360
samsundar <samradnus2001@gmail.com>
2022-05-23 23:32:12 +0530

    updated User Model and avatar

--- base/models.py
+++ base/models.py
@@ -1,7 +1,20 @@
+from pickle import TRUE
 from django.db import models
-from django.contrib.auth.models import User
-# Create your models here.
-# id for models are automatically generated /default :1,2
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    name=models.CharField(max_length=200,null=True)
+    email=models.EmailField(unique=True,null=True)
+    bio=models.TextField(null=True)
+     
+    avatar=models.ImageField(null=True,default="avatar.svg")
+
+    USERNAME_FIELD ='email'
+    REQUIRED_FIELDS =[]
+
+
+#Create your models here.
+#id for models are automatically generated /default :1,2
 class Topic(models.Model):
     name=models.CharField(max_length=200)
 
@@ -35,6 +48,6 @@ class Message(models.Model):
 
     class Meta:
         ordering = ['-updated','-created']  # '-' puts the new data field before all the prefilled data
-        
-    def __str__(self):
-        return self.body[0:50]
+     
+        def __str__(self):
+            return self.body[0:50]
