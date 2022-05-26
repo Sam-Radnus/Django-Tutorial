249c76c44c55b1c7444cca4f02c0bec6abed85aa
samsundar <samradnus2001@gmail.com>
2022-05-26 08:57:52 +0530

    Updated User Model

--- studybud/settings.py
+++ studybud/settings.py
@@ -44,6 +44,9 @@ INSTALLED_APPS = [
       'corsheaders',
 ]
 
+AUTH_USER_MODEL='base.User'
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
 
@@ -127,11 +130,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+MEDIA_URL  ='/images/'
 STATICFILES_DIRS=[
     BASE_DIR / 'static'
 ]
 
+MEDIA_ROOT=BASE_DIR / 'static/images'
 
 #STATIC_ROOT=
 # Default primary key field type
--- studybud/urls.py
+++ studybud/urls.py
@@ -1,6 +1,9 @@
 
+from xml.dom.minidom import Document
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 # from django.http import HttpResponse
 
@@ -17,3 +20,5 @@ urlpatterns = [
     path('api/',include('base.api.urls'))
 
 ]
+
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
